712d2d22b89a16aa73b526c15bc7c6606add6e4b
carlos <jnajerac3@miumg.edu.gt>
2023-05-16 00:09:24 -0600

    cambios a librerias locales para el funcionamiento offline

---
+++ includes/config.php
@@ -0,0 +1,17 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| inventario
+|--------------------------------------------------------------------------
+| Author: Jorge Carlos Najera Castañeda
+| Project Name: inventario
+|
+|
+|
+*/
+  define( 'DB_HOST', 'localhost' );          // establecer host de la base de datos
+  define( 'DB_USER', 'root' );             // establecer usuario de la db
+  define( 'DB_PASS', '' );             // establecer contraseña de la db
+  define( 'DB_NAME', 'sistema' );        // establecer nombre de la db
+
+?>
---
+++ layouts/footer.php
@@ -0,0 +1,12 @@
+     </div>
+    </div>
+    <script src="libs/js/jquery-1.11.2.js"></script>
+  <script src="bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
+  <script src="libs/js/bootstrap-datepicker.min.js"></script>
+  <script type="text/javascript" src="libs/js/functions.js"></script>
+  </body>
+</html>
+
+<?php if(isset($db)) { $db->db_disconnect(); } ?>
+
+
